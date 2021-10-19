fb422b5131480b46a25a18ff29ecebd183e95641
Helen Tan <helentanmz0909@gmail.com>
2021-10-19 10:56:11 +0800

    Render popup when marker is selected

--- src/App.js
+++ src/App.js
@@ -4,44 +4,40 @@ import Flat from "./components/Flat";
 import Search from "./components/Search";
 import Map from "./components/Map";
 
-const FLATS =[1];
 
 function App() {
   const [allFlats, setAllFlats] = useState([]);
   const [flats, setFlats] = useState([]);
-/*
-useEffect(() => {
-  fetch("https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json")
-  .then((res) => res.json())
-  .then((data) => setFlats(data));
-}, []);
-*/
+  const [selectedFlat, setSelectedFlat] = useState(null);
 
-useEffect(() => {
-  fetch("https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json")
-    .then((res) => res.json())
-    .then((flats) => {
-      setAllFlats(flats);
-      setFlats(flats);
-    });
-}, []);
+
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json")
+      .then((res) => res.json())
+      .then((flats) => {
+        setAllFlats(flats);
+        setFlats(flats);
+      });
+  }, []);
 
 
   return (
     <div className="App">
-        <main>
-          <div className="contents">
-            <div><Search/></div>
-            <div className="flat-wrapper">
-              {FLATS.map(flat => {
-                return <Flat key={flat.id} flat={flat}/>
-              })}
-            </div>
+      <main>
+        <div className="contents">
+          <div><Search /></div>
+
+          <div className="flats-wrapper">
+            {flats.length
+              ? flats.map((flat) => <Flat flat={flat} selectedFlat={selectedFlat} setSelectedFlat={setSelectedFlat} />)
+              : "No results found"}
           </div>
-          <div className="map">
-            <Map flats={flats}/>
-          </div>
-        </main>
+        </div>
+      </main>
+
+      <div className="map">
+        <Map flats={flats} selectedFlat={selectedFlat} setSelectedFlat={setSelectedFlat} />
+      </div>
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import ReactMapGL, { Marker } from "react-map-gl";
+import React, { useEffect, useState } from 'react';
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { FaMapMarkerAlt } from 'react-icons/fa'
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './Map.css';
@@ -9,15 +9,33 @@ const PARIS = {
     lng: 2.3522,
 }
 
-const Map = ({ flats }) => {
+const Map = ({ flats, selectedFlat, setSelectedFlat }) => {
     const accessToken = 
     "pk.eyJ1IjoiaGVsZW4tdGFuLW16IiwiYSI6ImNrdXYya2ludjA2N2kycHJmM2RxdTYwMmUifQ.JoDUdQbqD62h-AN4hIAwQw";
 
     const [viewport, setViewport] = useState({
+        width: "100%",
+        height: "100vh",
         latitude: PARIS.lat,
         longitude: PARIS.lng,
         zoom: 11
     });
+
+    useEffect(() => {
+        if(selectedFlat){
+            setViewport({
+                latitude: selectedFlat.lat,
+                longitude: selectedFlat.lng,
+                zoom: 15,
+            });
+        } else {
+            setViewport({
+                latitude: PARIS.lat,
+                longitude: PARIS.lng,
+                zoom: 11,
+            });
+        }
+    }, [selectedFlat]);
 
     return (
         <ReactMapGL
@@ -30,11 +48,30 @@ const Map = ({ flats }) => {
         >
             {flats.map((flat) => {
                 return(
-                    <Marker key={flat.id} latitude={flat.lat} longitude={flat.lng}>
+                    <Marker key={flat.id} latitude={flat.lat} longitude={flat.lng} onClick={() => setSelectedFlat(flat)}>
                         <FaMapMarkerAlt color="tomato" size="1.5em"/>
                     </Marker>
                 );
             })}
+            {/*
+            <Popup latitude={PARIS.lat} longitude={PARIS.lng}>
+                <div className="popup">
+                    <div className="price">€500</div>
+                    <div className="name"> Heart of paris</div>
+                </div>
+            </Popup>
+            */}
+
+            {/*Conditionally render popup only when there is a selectedFlat*/}
+            {selectedFlat && (
+                <Popup latitude={selectedFlat.lat} longitude={selectedFlat.lng} closeOnClick={true} onClose={()=> setSelectedFlat(null)}>
+                    <div className="popup">
+                        <div className="price">€{selectedFlat.price}</div>
+                        <div className="name">{selectedFlat.name}</div>
+                    </div>
+                </Popup>
+            )};
+            
         </ReactMapGL>
     );
 };
