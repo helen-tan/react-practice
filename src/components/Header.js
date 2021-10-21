0acade993879ef7b2514fdbb8342713e2d1d6777
Helen Tan <helentanmz0909@gmail.com>
2021-10-21 16:03:32 +0800

    Added reactstrap to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./Header.css";
-//import { Nav } from 'reactstrap';
+import { Nav } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
-    return(
-        <div className="header">FindProperty</div>
+    return (
+        <Nav className="header">
+            <div className="container">
+                <div className="row">
+                    <div className="logo">
+                        <div><img src=".\Logo.png" alt="logo"/> FindProperty</div>
+                    </div>
+                </div>
+            </div>
+        </Nav>
     );
 };
 
